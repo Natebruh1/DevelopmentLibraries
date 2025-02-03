4db6749a79f0a498a669b0a06b6edd7cd09e6a54
Natebruh1 <125579576+Natebruh1@users.noreply.github.com>
2025-02-03 10:53:00 +0000

    Vector3

---
+++ DevelopmentLibraries/MathVector.h
@@ -0,0 +1,51 @@
+#include <cmath>
+
+union vector3
+{
+	struct { float x, y, z; };
+	float v[3];
+	float length()
+	{
+		return std::sqrtf(x * x + y * y + z * z);
+	}
+	float lengthSquared()
+	{
+		return x * x + y * y + z * z;
+	}
+	//Returns the largest component
+	float max()
+	{
+		float m = x;
+		(m < y) && (m = y); //these are not conditional statements.
+		(m < z) && (m = z); //these are just boolean expressions.
+		return m;
+		
+	}
+	//Returns the largest component index
+	float maxIndex()
+	{
+		return (y > x) * ((y > x) + (z > y)) + ((x>y)*(z>x)*2);
+	}
+
+
+	float dist(vector3 o)
+	{
+		return std::sqrtf((o.x - x) * (o.x - x) + (o.y - y) * (o.y - y) + (o.z - z) * (o.z - z));
+	}
+	float distSquared(vector3 o)
+	{
+		return (o.x - x) * (o.x - x) + (o.y - y) * (o.y - y) + (o.z - z) * (o.z - z);
+	}
+	vector3 operator-(vector3 const& rhs)
+	{
+		return { x - rhs.x,y - rhs.y,z - rhs.z };
+	}
+	vector3 operator+(vector3 const& rhs)
+	{
+		return { x + rhs.x,y + rhs.y,z + rhs.z };
+	}
+	vector3 operator*(vector3 const& rhs)
+	{
+		return { x * rhs.x,y * rhs.y,z * rhs.z };
+	}
+};
--- DevelopmentLibraries/Source.cpp
+++ DevelopmentLibraries/Source.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include "EnumFlags.h"
+#include "MathVector.h"
 //EnumFlag auto g=Colours::Red;
-
+int x = 0;
 int main()
 {
 	Colours g = Colours::Red|Colours::Blue;
+	if (x) [[likely]]
+	{
 
-	std::cout << (int)g << std::endl;
+	}
+	//std::cout << (int)g << std::endl;
+
+
+	vector3 a = { 4,12,8 };
+	vector3 b = { 8,1,3 };
+	std::cout << (a*b).x << std::endl;
+
 	return 0;
 }
