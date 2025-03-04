874f521c0d2ee91d99efdb37ee9ebfae3d145533
Natebruh1 <125579576+Natebruh1@users.noreply.github.com>
2025-03-04 10:53:30 +0000

    Erased Class

--- DevelopmentLibraries/Source.cpp
+++ DevelopmentLibraries/Source.cpp
@@ -2,8 +2,32 @@
 #include "EnumFlags.h"
 #include "MathVector.h"
 #include "MathDerivative.h"
+#include "Erased.h"
 //EnumFlag auto g=Colours::Red;
 int x = 0;
+
+
+struct Draw
+{
+	static void invoker(const auto& self, std::string& a)
+	{
+		self.draw(a);
+	}
+	// not necessary, but makes the client code easier to write
+	void draw(this const auto& erased, auto& stream)
+	{
+		erased.invoke(Draw{}, stream);
+	}
+};
+
+using Drawable = erased::erased<Draw>;
+
+struct Circle {
+	void draw(std::string& s) const { std::cout << s; }
+};
+struct Square {
+	void draw(std::string& s) const { std::cout << "Square" << std::endl;; }
+};
 
 
 Dual testDer(Dual x)
@@ -29,6 +53,11 @@ int main()
 	
 	std::cout << diff(&testDer, a).x;
 
+	std::string strIn = ("Input");
+	Drawable drawable = Circle{};
+	drawable.draw(strIn);
+	drawable = Square{};
+	drawable.draw(strIn);
 	return 0;
 }
 
